605a03de006f50a7feae567fc7141e835abb4b1c
= <=>
2024-09-14 08:15:43 +0200

    add health check for db container

--- start_services.sh
+++ start_services.sh
@@ -4,10 +4,10 @@
 gunicorn --bind 0.0.0.0:8000 webapp.wsgi:application &
 
 # Celery worker
-celery -A webapp worker --loglevel=DEBUG --logfile=celery_worker.log &
+# celery -A webapp worker --loglevel=DEBUG --logfile=celery_worker.log &
 
 # Celery beat
-celery -A webapp beat --loglevel=DEBUG --logfile=celery_beat.log &
+# celery -A webapp beat --loglevel=DEBUG --logfile=celery_beat.log &
 
 # Wait for all background processes to finish
 wait
